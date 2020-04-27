b8d516f524cba513c56ef726a0b31ae0d96d8b95
kansald006 <41112606+kansald006@users.noreply.github.com>
2020-04-27 14:50:45 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/corona-dash'));
+
+app.all('*', (req, res) => {
+    res.status(200).sendFile(__dirname + '/dist/corona-dash/index.html');
+});
+
+app.listen(process.env.PORT || 8080);
